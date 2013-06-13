f00b300aa824df3677dc8ccf0e19c0ddd2707659
ralfzen <ralfzen@gmail.com>
2013-06-13 22:35:25 +0200

    moved static into js -folder

--- inline_ordering/default_settings.py
+++ inline_ordering/default_settings.py
@@ -1,8 +1,12 @@
 from django.conf import settings
+import os
+
 
 # path to inline_ordering.js
-INLINE_ORDERING_JS_URL = getattr(settings, 'INLINE_ORDERING_JS_URL', settings.STATIC_URL + 'inline_ordering.js')
+INLINE_ORDERING_JS_URL = getattr(settings, 'INLINE_ORDERING_JS_URL', 
+    os.path.join(settings.STATIC_URL, 'js', 'inline_ordering.js'))
 
-
-JQUERY_PREAMBLE_JS_URL = INLINE_ORDERING_JS_URL.replace('inline_ordering.js', 'inline_ordering_preamble.js')
-JQUERY_UI_JS_URL = INLINE_ORDERING_JS_URL.replace('inline_ordering.js', 'jquery-ui.min.js')
+JQUERY_PREAMBLE_JS_URL = INLINE_ORDERING_JS_URL.replace('inline_ordering.js',
+    'inline_ordering_preamble.js')
+JQUERY_UI_JS_URL = INLINE_ORDERING_JS_URL.replace('inline_ordering.js', 
+    'jquery-ui.min.js')
